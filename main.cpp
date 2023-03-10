89b06b83bb22cea22265adafc742d027faa61f38
Zachary Canann <zacharycanann@gmail.com>
2023-03-10 13:26:18 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <set>
+#include <vector>
+
+// Return an integer that represents (a * b) % n without using the % operator
+int CalculateModOfProduct(int a, int b, int n)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    // Example call
+    std::cout << CalculateModOfProduct(2, 2, 3) << std::endl;
+
+    return EXIT_SUCCESS;
+}
