bb53ad6374c01c4f31d552c7feee2d4d6b1d572c
Zachary Canann <zacharycanann@gmail.com>
2023-07-11 09:47:27 -0700

    Print formatting

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ int CalculateModOfProduct(int a, int b, int n)
 
 int main(int argc, char *argv[])
 {
-    std::cout << CalculateModOfProduct(2, 3, 4) << " (expected: 2)" << std::endl;
-    std::cout << CalculateModOfProduct(1, 5, 2) << " (expected: 1)" << std::endl;
-    std::cout << CalculateModOfProduct(9, 1, 5) << " (expected: 4)" << std::endl;
+    std::cout << CalculateModOfProduct(2, 3, 4) << "\t(expected: 2)" << std::endl;
+    std::cout << CalculateModOfProduct(1, 5, 2) << "\t(expected: 1)" << std::endl;
+    std::cout << CalculateModOfProduct(9, 1, 5) << "\t(expected: 4)" << std::endl;
 
     return EXIT_SUCCESS;
 }
